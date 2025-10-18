91aec9a7800d5f5f5d7705f121e8e25f9e0007e9
James Rising <jarising@gmail.com>
2025-10-17 21:29:51 -0400

    working on config

--- src/config.py
+++ src/config.py
@@ -1,47 +1,43 @@
 from lib import commands, checks
 
-count_perrun = 21000
-openai_config = 'batch'
-gemini_config = 'fast'
+count_perrun = 25
+openai_config = 'slow' #'batch'
+gemini_config = 'slow' #'fast'
 
-searches = ["/Users/admin/Library/CloudStorage/GoogleDrive-jrising@udel.edu/My Drive/Research/COP30 Ada/AI Review/scopus.csv",
-            "/Users/admin/Library/CloudStorage/GoogleDrive-jrising@udel.edu/My Drive/Research/COP30 Ada/AI Review/savedrecsII.xls"]
-response_file = "../responses.csv"
-verdict_file = "../verdicts.csv"
+searches = ["../test/macroecono.xlsx"]
+response_file = "../test/responses.csv"
+verdict_file = "../test/verdicts.csv"
 
-abstract_prompt = "I am performing a global review and analytical synthesis of the macroeconomic and macro-fiscal risks of climate change and the costs and benefits of adaptation at a national level."
-exclude_codes = {'XC': "Not related to climate risks",
-                 'XV': "Not valued in economic terms",
-                 'XN': "Not nation-scale or multinational (not macroeconomic or related to nation-wide adaptation)",
-                 'XA': "No new analysis (using previously-published work)",
-                 'XO': "Excluded for another reason (please specify)"}
-include_codes = {'RE': "Reports macroeconomic outcomes (GDP effects, productivity loss, interest, inflation, etc.)",
-                 'RF': "Reports fiscal outcomes (budget balance, borrowing costs, public expenditure or revenue, etc.)",
-                 'RA': "Reports both economic costs and/or benefits (or related measures like ROI, NPV) of nation-wide adaptation"}
+abstract_prompt = "I am performing a meta-analysis of econometric models of temperature on economic growth. These papers use regression-based techniques to understand how variation in weather results in variation in GDP growth rates. I am only interested in papers that do this globally, with at least national-level resolution."
+exclude_codes = {'XG': "Not GDP growth (studying a different dependent variable)",
+                 'XE': "Not econometric (using non-causal or non-statistical methods)",
+                 'XW': "Not global (region-specific)",
+                 'XN': "No new empirics (using previously-published work)"}
+include_codes = {'PL': "Plausibly appropriate: Unlikely",
+                 'PM': "Plausibly appropriate: Somewhat likely",
+                 'PH': "Plausibly appropriate: Very likely"}
 
 ## Question
 questionsource = verdict_file.replace(".csv", "-further.csv")
-singlequestion = "Based on this information, does this paper describe outcomes driven by climate change (that is, all the effects that come from increased greenhouse gas concentrations, such as increased temperatures and other weather changes), as opposed to the effects of climate policy? I am also interested in adaptation and resilience, but again, I do *not* want to consider reductions in losses that are coming from reduced greenhouse gas emissions."
-question_file = "../question.csv"
+singlequestion = "Based on this information, does this paper describe outcomes driven by climate change (that is, all the effects that come from increased greenhouse gas concentrations, such as increased temperatures and other weather changes), as opposed to the effects of climate policy? I do *not* want to consider reductions in losses that are driven by mitigation policy alone."
+question_file = "../test/question.csv"
 
 ## Find PDFs
 priority_limit = 8
 pdfs_dir = "../pdfs"
-finder_count = 100
+finder_count = 25
 
 ## Extract data from PDFs
-extract_dir = "../extract"
-collate_count = 100
-column_defs_collate = {'Outcome(s) of Interest': "Specific macroeconomic outcomes (GDP effects, productivity loss, interest, inflation, etc.) or fiscal outcomes (budget balance, borrowing costs, public expenditure or revenue, etc.) or measures of economic costs and/or benefits (or related measures like ROI, NPV) of nation-wide adaptation.",
-                       'Drivers of Risk': "What shocks or changes are impacting the outcomes?",
-                       'Quantitative material': "Is there quantitative information on the outcomes of interest on this page?",
+extract_dir = "../test/extract"
+collate_count = 25
+column_defs_collate = {'Quantitative material': "Is there quantitative information on the outcomes of interest on this page?",
                        'Methodology': "How is the analysis performed and when are its results applicable?",
                        'Highlights': "What are the most important outcomes of the analysis?",
                        'More Notes': "Do you have any other notes?"}
 
 ## Summarized line for PDF
-summary_count = 100
-summary_file = "../summary.csv"
+summary_count = 25
+summary_file = "../test/summary.csv"
 column_defs_summary = {'All': {
     'Author(s)': lambda row, xtt: commands.short_authors(row['Authors']),
     'Year': lambda row, xtt: row['Year'],
@@ -50,7 +46,7 @@ column_defs_summary = {'All': {
     'Paper ID': lambda row, xtt: row['DOI'],
     'Reviewer': "AI",
     'Status': "STATUS",
-    'NEXT': lambda row, xtt: commands.ai_select(xtt, "Does this paper produce new quantitative results on specific macroeconomic outcomes (GDP effects, productivity loss, interest, inflation, etc.) or fiscal outcomes (budget balance, borrowing costs, public expenditure or revenue, etc.) or measures of economic costs and/or benefits (or related measures like ROI, NPV) of nation-wide adaptation?", ['Quantitative material', 'Methodology'], ['Macroeconomic', 'Fiscal', 'Adaptation'], abstract_prompt)},
+    'NEXT': lambda row, xtt: commands.ai_select(xtt, "Does this paper produce new econometric results describing GDP growth rates or the extent of persistence?", ['Quantitative material', 'Methodology'], ['Macroeconomic', 'Fiscal', 'Adaptation'], abstract_prompt)},
                        'Any': {
                            'Country(ies)': lambda row, xtt: commands.ai_summary(xtt, "What specific country or group of countries was considered? Just provide the country names or group.", ['Methodology'], abstract_prompt),
                            'Outcome(s) of Interest': "BRIEF",
